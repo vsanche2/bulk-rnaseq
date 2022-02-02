1120725cdc8641bb7677f01da081448f8949e350
Shweta Chavan <chavan.shweta@gmail.com>
2022-02-02 15:33:35 -0500

    Update 1-master-star.sh

--- 1-master-star.sh
+++ 1-master-star.sh
@@ -1,11 +1,65 @@
+#!/bin/bash
+conda activate rnaseq
+~/get_a_worker_node.sh ##Don't forget to get a worker node first!!! Do not run this on the main node
 
+IN_SS=SampleSheet.txt
+OUT_DIR=RUN2
+IN_DIR=NEO-fastqs
+TRIM_FASTQ_DIR=RUN1
 
+#For STAR
+GTF=""
+KALLISTO_IDX=~/projects/bulk_rna_seq/kallisto/genome/homo_sapiens/transcriptome.idx
+
+##Edit block above to point to appropriate locations
+##-----------------------------------------------------------------------------------------------------------------------
+
+echo "Input dir: "$IN_DIR
+echo "Output dir: "$OUT_DIR
+date
+
+while IFS=$'\t' read line
+do
+	set $line
+    SAMPLE=$1
+    COND=$2
+    echo "Started working on: "$SAMPLE $COND
+
+#Merge Lanes
+cat $IN_DIR/$SAMPLE*R1* > ${OUT_DIR}/${SAMPLE}.${COND}.merged.R1.fastq.gz
+cat $IN_DIR/$SAMPLE*R2* > ${OUT_DIR}/${SAMPLE}.${COND}.merged.R2.fastq.gz
+
+#Run fastp
+fastp -h ${OUT_DIR}/${SAMPLE}.${COND}.fastp.html -j ${OUT_DIR}/${SAMPLE}.${COND}.fastp.json \
+-i ${OUT_DIR}/${SAMPLE}.${COND}.merged.R1.fastq.gz -I ${OUT_DIR}/${SAMPLE}.${COND}.merged.R2.fastq.gz \
+-o ${OUT_DIR}/${SAMPLE}.${COND}.merged.trimmed.R1.fastq.gz -O ${OUT_DIR}/${SAMPLE}.${COND}.merged.trimmed.R2.fastq.gz \
+--dedup \
+--cut_front \
+--cut_front_window_size \
+--cut_tail_window_size \
+--cut_tail \
+--n_base_limit \
+--length_required 40 \
+--low_complexity_filter \
+--overrepresentation_analysis
+#-y -3 --cut_tail_window_size 4 -5 --cut_front_window_size 4 --length_required 40
+
+#Run fastp
+fastqc $OUT_DIR
+
+#Run STAR
+mkdir -p ${OUT_DIR}/STAR/${SAMPLE}.${COND}
+
+kallisto quant -i $KALLISTO_IDX -o ${OUT_DIR}/QUANT/${SAMPLE}.${COND} --pseudobam --bias -b 100 -t 8 --fusion ${OUT_DIR}/${SAMPLE}.${COND}.merged.trimmed.R1.fastq.gz ${OUT_DIR}/${SAMPLE}.${COND}.merged.trimmed.R2.fastq.gz &> ${OUT_DIR}/QUANT/${SAMPLE}.${COND}/${SAMPLE}.${COND}.kallisto.log
 
 #Alignment QC
-qualimap rnaseq \
--outdir results/qualimap/Mov10_oe_1 \
--a proportional \
--bam results/STAR/Mov10_oe_1_Aligned.sortedByCoord.out.bam \
--p strand-specific-reverse \
--gtf /n/groups/hbctraining/intro_rnaseq_hpc/reference_data_ensembl38/Homo_sapiens.GRCh38.92.1.gtf \
---java-mem-size=8G
+qualimap rnaseq -outdir $OUT_DIR -a proportional -bam ${OUT_DIR}/STAR/${SAMPLE}.${COND}.bam -p strand-specific-reverse -gtf $GTF --java-mem-size=8G
+
+#Run MultiQC
+multiqc -f $OUT_DIR ${OUT_DIR}/STAR/
+
+done < <(cat $IN_SS)
+date
+
+
+
